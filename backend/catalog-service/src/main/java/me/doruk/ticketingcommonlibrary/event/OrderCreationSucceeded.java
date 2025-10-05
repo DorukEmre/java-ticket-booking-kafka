d5fe7305dcd177c094e44d64268c25e55d708e5e
Doruk Emre <doruk.emre@outlook.com>
2025-10-05 09:51:45 +0200

    Implement OrderCreation event classes

--- backend/catalog-service/src/main/java/me/doruk/ticketingcommonlibrary/event/OrderCreationSucceeded.java
+++ backend/catalog-service/src/main/java/me/doruk/ticketingcommonlibrary/event/OrderCreationSucceeded.java
@@ -1,5 +1,22 @@
 package me.doruk.ticketingcommonlibrary.event;
 
-public class OrderCreationSucceeded {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.doruk.ticketingcommonlibrary.model.CartItem;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderCreationSucceeded {
+  UUID cartId;
+  Long orderId;
+  BigDecimal totalPrice;
+  List<CartItem> items;
 }
--- backend/order-service/src/main/java/me/doruk/ticketingcommonlibrary/event/OrderCreationFailed.java
+++ backend/order-service/src/main/java/me/doruk/ticketingcommonlibrary/event/OrderCreationFailed.java
@@ -1,5 +1,17 @@
 package me.doruk.ticketingcommonlibrary.event;
 
-public class OrderCreationFailed {
+import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderCreationFailed {
+  UUID cartId;
+  Long orderId;
 }
