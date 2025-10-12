1a2e0ed475b8dac145fa910e5c877514315f0ca2
Doruk Emre <doruk.emre@outlook.com>
2025-10-12 09:38:24 +0200

    Add VenueCard component

---
+++ frontend/src/components/VenueCard.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+import type { Venue } from "@/types/catalog";
+import { imageBaseUrl } from "@/utils/globals";
+
+function VenueCard({ venue, overlayColour }
+  : { venue: Venue, overlayColour: { background: string } }) {
+
+  return (
+    <>
+      <li className="card venue_card__image">
+        <img src={`${imageBaseUrl}${venue.imageUrl}`} alt="" className="card-img" />
+        <div
+          style={overlayColour}
+          className="card-img-overlay venue_card__overlay"
+          aria-hidden="true"
+        />
+        <Link to={`/venues/${venue.venueId}`} className="card-img-overlay d-flex flex-column text-white text-decoration-none align-items-center justify-content-center">
+          <p className="fs-3">{venue.name}</p>
+          <p className="fs-3">{venue.location}</p>
+        </Link>
+      </li>
+    </>
+  )
+}
+
+export default VenueCard;
--- frontend/src/components/VenueList.tsx
+++ frontend/src/components/VenueList.tsx
@@ -1,23 +1,37 @@
+import VenueCard from '@/components/VenueCard';
+
 import type { Venue } from '@/types/catalog';
-import { Link } from 'react-router-dom';
 
-type VenueListProps = {
-  venues: Venue[];
-};
+function VenueList({ venues }: { venues: Venue[] }) {
 
-function VenueList({ venues }: VenueListProps) {
+  // Sort 6 venues randomly
+  const displayedEvents = [...venues]
+    .sort(() => (0.5 - Math.random()))
+    .slice(0, 6);
+
+  const colours = [
+    { background: 'linear-gradient(90deg, rgba(10, 102, 163, 0.9) 0%, rgba(101, 149, 211, 0.9) 100%)' },
+    { background: 'linear-gradient(90deg, rgba(12, 175, 80, 0.9) 0%, rgba(65, 171, 109, 0.9) 100%)' },
+    { background: 'linear-gradient(90deg, rgba(150, 68, 183, 0.9) 0%, rgba(161, 129, 174, 0.9) 100%)' },
+    { background: 'linear-gradient(90deg, rgba(243, 156, 18, 0.9) 0%, rgba(214, 174, 95, 0.9) 100%)' },
+    { background: 'linear-gradient(90deg, rgba(9, 160, 130, 0.9) 0%, rgba(87, 161, 147, 0.9) 100%)' },
+    { background: 'linear-gradient(90deg, rgba(231, 62, 43, 0.9) 0%, rgba(197, 109, 99, 0.9) 100%)' },
+  ];
+
+  // create a new array with the colours  randomly ordered
+  const shuffledColours = [...colours].sort(() => 0.5 - Math.random());
+
   return (
-    <ul>
-      {venues.map(venue => (
-        <li key={venue.venueId}>
-          <Link to={`/venues/${venue.venueId}`}>
-            Venue ID {venue.venueId}: {venue.name}, {venue.location} - {venue.totalCapacity}
-          </Link>
-        </li>
+    <ul className="d-flex list-unstyled flex-wrap justify-content-center justify-content-md-between align-items-stretch gap-4">
+      {displayedEvents.map(venue => (
+        <VenueCard
+          key={venue.venueId}
+          venue={venue}
+          overlayColour={shuffledColours[displayedEvents.indexOf(venue)]}
+        />
       ))}
     </ul>
   )
-
 }
 
 export default VenueList;
