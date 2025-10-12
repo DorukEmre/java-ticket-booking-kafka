592ea574cc6d13a7ac6c74f2fd9bd05e00504536
Doruk Emre <doruk.emre@outlook.com>
2025-10-12 11:07:30 +0200

    Add index file for image imports

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,18 @@
+import addIcon
+  from "@/assets/icons/add_24dp_C5C0B9_FILL0_wght400_GRAD0_opsz24.svg"
+import deleteIcon
+  from "@/assets/icons/delete_24dp_C5C0B9_FILL0_wght400_GRAD0_opsz24.svg"
+import removeIcon
+  from "@/assets/icons/remove_24dp_C5C0B9_FILL0_wght400_GRAD0_opsz24.svg"
+import cartIcon
+  from "@/assets/icons/shopping_cart_24dp_C5C0B9_FILL0_wght400_GRAD0_opsz24.svg"
+
+import debitCard from "@/assets/debit_card.png"
+
+export {
+  addIcon,
+  deleteIcon,
+  removeIcon,
+  cartIcon,
+  debitCard,
+};
