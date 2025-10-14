e442b99bf1a71b87b085d3716877752e407023e7
Doruk Emre <doruk.emre@outlook.com>
2025-10-14 10:21:44 +0200

    Update HeaderCartButton to display total ticket count from cart

--- frontend/src/components/HeaderCartButton.tsx
+++ frontend/src/components/HeaderCartButton.tsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
 
 import { useCart } from "@/hooks/useCart";
+import { useEffect, useState } from "react";
 
 function HeaderCartButton({ path, text, icon }
   : { path: string; text: string, icon?: string }) {
 
   const { cart } = useCart();
+  const [totalTicketCount, setTotalTicketCount] = useState(0);
+
+  useEffect(() => {
+    // calculte total ticket count from cart
+
+    let count = 0;
+
+    for (const item of cart?.items || []) {
+      cart?.items && (count += item.ticketCount);
+    }
+
+    setTotalTicketCount(count);
+
+  }, [cart?.items]);
 
   return (
     <li className="nav-item p-2 rounded header-button">
@@ -14,7 +29,7 @@ function HeaderCartButton({ path, text, icon }
         {icon && <img src={icon} alt="icon" aria-hidden="true" />}
         {(cart?.items?.length || 0) > 0 &&
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {cart?.items?.length}
+            {totalTicketCount}
             <span className="visually-hidden">unread messages</span>
           </span>
         }
