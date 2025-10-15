56fc5bf472f06d499887dc7f414038628c88062d
Doruk Emre <doruk.emre@outlook.com>
2025-10-15 18:58:42 +0200

    Split CatalogService into smaller files

--- backend/catalog-service/src/main/java/me/doruk/catalogservice/controller/CatalogController.java
+++ backend/catalog-service/src/main/java/me/doruk/catalogservice/controller/CatalogController.java
@@ -4,7 +4,9 @@ import me.doruk.catalogservice.request.EventCreateRequest;
 import me.doruk.catalogservice.request.VenueCreateRequest;
 import me.doruk.catalogservice.response.EventResponse;
 import me.doruk.catalogservice.response.VenueResponse;
-import me.doruk.catalogservice.service.CatalogService;
+import me.doruk.catalogservice.service.EventService;
+import me.doruk.catalogservice.service.InventoryService;
+import me.doruk.catalogservice.service.VenueService;
 import me.doruk.ticketingcommonlibrary.model.Cart;
 
 import org.springframework.http.HttpStatus;
@@ -24,49 +26,51 @@ import java.util.Map;
 @RequestMapping("api/v1")
 public class CatalogController {
 
-  private final CatalogService catalogService;
+  private final InventoryService inventoryService;
+  private final EventService eventService;
+  private final VenueService venueService;
 
   @GetMapping("/catalog/events/{eventId}")
   public @ResponseBody EventResponse catalogByEventId(@PathVariable("eventId") Long eventId) {
     System.out.println("GET /api/v1/catalog/events/" + eventId + " called");
-    return catalogService.getEventInformation(eventId);
+    return eventService.getEventInformation(eventId);
   }
 
   @GetMapping("/catalog/events")
   public @ResponseBody List<EventResponse> catalogGetAllEvents() {
     System.out.println("GET /api/v1/catalog/events called");
-    return catalogService.GetAllEvents();
+    return eventService.GetAllEvents();
   }
 
   @GetMapping("/catalog/venues/{venueId}")
   public @ResponseBody VenueResponse catalogByVenueId(@PathVariable("venueId") Long venueId) {
     System.out.println("GET /api/v1/catalog/venues/" + venueId + " called");
-    return catalogService.getVenueInformation(venueId);
+    return venueService.getVenueInformation(venueId);
   }
 
   @GetMapping("/catalog/venues")
   public @ResponseBody List<VenueResponse> catalogGetAllVenues() {
     System.out.println("GET /api/v1/catalog/venues called");
-    return catalogService.getAllVenues();
+    return venueService.getAllVenues();
   }
 
   @PostMapping(value = "/catalog/add-venue", consumes = "application/json", produces = "application/json")
   public ResponseEntity<VenueResponse> createVenue(@RequestBody @Valid VenueCreateRequest request) {
     System.out.println("POST /api/v1/catalog/add-venue called");
-    VenueResponse createdVenue = catalogService.createVenue(request);
+    VenueResponse createdVenue = venueService.createVenue(request);
     return ResponseEntity.status(HttpStatus.CREATED).body(createdVenue);
   }
 
   @PostMapping(value = "/catalog/add-event", consumes = "application/json", produces = "application/json")
   public ResponseEntity<EventResponse> createEvent(@RequestBody @Valid EventCreateRequest request) {
     System.out.println("POST /api/v1/catalog/add-event called");
-    EventResponse createdEvent = catalogService.createEvent(request);
+    EventResponse createdEvent = eventService.createEvent(request);
     return ResponseEntity.status(HttpStatus.CREATED).body(createdEvent);
   }
 
   @PostMapping("/catalog/validate-cart")
   public ResponseEntity<Map<Long, Boolean>> validateCart(@RequestBody Cart cart) {
-    Map<Long, Boolean> result = catalogService.validateCart(cart);
+    Map<Long, Boolean> result = inventoryService.validateCart(cart);
     return ResponseEntity.ok(result);
   }
 }
--- backend/catalog-service/src/main/java/me/doruk/catalogservice/service/InventoryService.java
+++ backend/catalog-service/src/main/java/me/doruk/catalogservice/service/InventoryService.java
@@ -3,13 +3,7 @@ package me.doruk.catalogservice.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.doruk.catalogservice.entity.Event;
-import me.doruk.catalogservice.entity.Venue;
 import me.doruk.catalogservice.repository.EventRepository;
-import me.doruk.catalogservice.repository.VenueRepository;
-import me.doruk.catalogservice.request.EventCreateRequest;
-import me.doruk.catalogservice.request.VenueCreateRequest;
-import me.doruk.catalogservice.response.EventResponse;
-import me.doruk.catalogservice.response.VenueResponse;
 import me.doruk.ticketingcommonlibrary.event.InventoryReleaseRequested;
 import me.doruk.ticketingcommonlibrary.event.InventoryReservationResponse;
 import me.doruk.ticketingcommonlibrary.event.ReserveInventory;
@@ -18,15 +12,12 @@ import me.doruk.ticketingcommonlibrary.kafka.Topics;
 import me.doruk.ticketingcommonlibrary.model.Cart;
 import me.doruk.ticketingcommonlibrary.model.CartItem;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -35,123 +26,10 @@ import java.util.Map;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class CatalogService {
+public class InventoryService {
 
   private final EventRepository eventRepository;
-  private final VenueRepository venueRepository;
   private final KafkaTemplate<String, Object> kafkaTemplate;
-
-  public EventResponse getEventInformation(final Long eventId) {
-    final Event event = eventRepository.findById(eventId)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found"));
-
-    return EventResponse.builder()
-        .id(event.getId())
-        .name(event.getName())
-        .capacity(event.getRemainingCapacity())
-        .venue(event.getVenue())
-        .ticketPrice(event.getTicketPrice())
-        .eventDate(String.valueOf(event.getEventDate()))
-        .description(event.getDescription())
-        .imageUrl(event.getImageUrl())
-        .build();
-  }
-
-  public List<EventResponse> GetAllEvents() {
-    final List<Event> events = eventRepository.findAll();
-
-    return events.stream().map(event -> EventResponse.builder()
-        .id(event.getId())
-        .name(event.getName())
-        .capacity(event.getRemainingCapacity())
-        .venue(event.getVenue())
-        .ticketPrice(event.getTicketPrice())
-        .eventDate(String.valueOf(event.getEventDate()))
-        .description(event.getDescription())
-        .imageUrl(event.getImageUrl())
-        .build()).toList();
-  }
-
-  public VenueResponse getVenueInformation(final Long venueId) {
-    System.out.println("Fetching venue information for venueId: " + venueId);
-    final Venue venue = venueRepository.findById(venueId)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Venue not found"));
-
-    return VenueResponse.builder()
-        .id(venue.getId())
-        .name(venue.getName())
-        .location(venue.getLocation())
-        .totalCapacity(venue.getTotalCapacity())
-        .imageUrl(venue.getImageUrl())
-        .build();
-  }
-
-  public List<VenueResponse> getAllVenues() {
-    final List<Venue> venues = venueRepository.findAll();
-
-    return venues.stream().map(venue -> VenueResponse.builder()
-        .id(venue.getId())
-        .name(venue.getName())
-        .location(venue.getLocation())
-        .totalCapacity(venue.getTotalCapacity())
-        .imageUrl(venue.getImageUrl())
-        .build()).toList();
-  }
-
-  public VenueResponse createVenue(final VenueCreateRequest request) {
-    System.out.println("Creating venue: " + request);
-    Venue venue = new Venue();
-    venue.setName(request.getName());
-    venue.setLocation(request.getLocation());
-    venue.setTotalCapacity(request.getTotalCapacity());
-
-    Venue savedVenue = venueRepository.save(venue);
-
-    return VenueResponse.builder()
-        .id(savedVenue.getId())
-        .name(savedVenue.getName())
-        .location(savedVenue.getLocation())
-        .totalCapacity(savedVenue.getTotalCapacity())
-        .imageUrl(savedVenue.getImageUrl())
-        .build();
-  }
-
-  public EventResponse createEvent(final EventCreateRequest request) {
-    System.out.println("Creating event: " + request);
-
-    final Venue venue = venueRepository.findById(request.getVenueId())
-        .orElseThrow(() -> new ResponseStatusException(
-            HttpStatus.NOT_FOUND, "Venue with ID " + request.getVenueId() + " does not exist."));
-
-    if (request.getTotalCapacity() > venue.getTotalCapacity()) {
-      throw new ResponseStatusException(
-          HttpStatus.BAD_REQUEST, "Event capacity cannot exceed venue capacity.");
-    }
-
-    Event event = Event.builder()
-        .name(request.getName())
-        .totalCapacity(request.getTotalCapacity())
-        .remainingCapacity(request.getTotalCapacity())
-        .venue(venue)
-        .ticketPrice(request.getTicketPrice())
-        .eventDate(Date.valueOf(request.getEventDate()))
-        .description(request.getDescription())
-        .imageUrl(request.getImageUrl())
-        .build();
-
-    Event savedEvent = eventRepository.save(event);
-
-    return EventResponse.builder()
-        .id(savedEvent.getId())
-        .name(savedEvent.getName())
-        .capacity(savedEvent.getRemainingCapacity())
-        .venue(savedEvent.getVenue())
-        .ticketPrice(savedEvent.getTicketPrice())
-        .eventDate(String.valueOf(savedEvent.getEventDate()))
-        .description(savedEvent.getDescription())
-        .imageUrl(savedEvent.getImageUrl())
-        .build();
-  }
 
   // Validate cart from cart-service
   public Map<Long, Boolean> validateCart(final Cart cart) {
