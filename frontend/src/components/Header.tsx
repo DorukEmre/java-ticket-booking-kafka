1634e587f13cfc85fe9c9394a6ab65a58577015e
Doruk Emre <doruk.emre@outlook.com>
2025-10-17 14:20:32 +0200

    Fix menu not collapsing

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,32 +1,38 @@
 import { Link, useLocation } from "react-router-dom"
-import { Collapse } from "bootstrap"
 
 import HeaderButton from "@/components/HeaderButton"
 import HeaderCartButton from "@/components/HeaderCartButton"
 
 import { cartIcon } from "@/assets"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function Header() {
 
   const location = useLocation()
+  const [pathIsCheckout, setPathIsCheckout] = useState(false);
 
-  const handleClick = () => {
+  const closeNavbar = () => {
     const navbar = document.getElementById('navbarNav');
+
     if (!navbar) return;
 
     if (navbar.classList.contains('show')) {
-      try {
-        const inst = Collapse.getOrCreateInstance(navbar);
-        inst.hide();
-      } catch (e) {
-        navbar.classList.remove('show');
-      }
+      navbar.classList.remove('show');
     }
   }
 
   useEffect(() => {
-    handleClick()
+
+    // Close navbar on path change
+    closeNavbar()
+
+    // Hide header links if path is checkout
+    if (location.pathname.startsWith("/checkout")) {
+      setPathIsCheckout(true);
+    } else {
+      setPathIsCheckout(false);
+    }
+
   }, [location.pathname])
 
   return (
@@ -34,22 +40,32 @@ function Header() {
       <nav className="navbar navbar-expand-md bg-back-200" data-bs-theme="dark">
         <div className="container-fluid">
 
-          <Link className="navbar-brand text-neutral-300 p-3 text-decoration-none" to={"/"}>
-            Ticket Booking
-          </Link>
+          {!pathIsCheckout ? (
+            <Link className="navbar-brand text-neutral-300 p-3 text-decoration-none" to={"/"}>
+              Ticket Booking
+            </Link>
+          ) : (
+            <span className="navbar-brand text-neutral-300 p-3">
+              Ticket Booking
+            </span>
+          )}
 
-          <button className="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          {!pathIsCheckout && (
+            <>
+              <button className="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
+                <span className="navbar-toggler-icon"></span>
+              </button>
 
-          <div className="collapse navbar-collapse flex-grow-0" id="navbarNav">
-            <ul className="navbar-nav align-items-end gap-2 me-3">
-              <HeaderButton path={"/events"} text={"Events"} handleClick={handleClick} />
-              <HeaderButton path={"/venues"} text={"Venues"} handleClick={handleClick} />
-              <HeaderButton path={"/orders"} text={"My orders"} handleClick={handleClick} />
-              <HeaderCartButton path={"/cart"} text={"Cart"} icon={cartIcon} handleClick={handleClick} />
-            </ul>
-          </div>
+              <div className="collapse navbar-collapse flex-grow-0" id="navbarNav">
+                <ul className="navbar-nav align-items-end gap-2 me-3">
+                  <HeaderButton path={"/events"} text={"Events"} handleClick={closeNavbar} />
+                  <HeaderButton path={"/venues"} text={"Venues"} handleClick={closeNavbar} />
+                  <HeaderButton path={"/orders"} text={"My orders"} handleClick={closeNavbar} />
+                  <HeaderCartButton path={"/cart"} text={"Cart"} icon={cartIcon} handleClick={closeNavbar} />
+                </ul>
+              </div>
+            </>
+          )}
 
         </div>
       </nav>
