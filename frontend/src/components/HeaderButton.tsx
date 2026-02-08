0a597f2fc470776289bf1d0cae49fc26740a01bf
Doruk Emre <doruk.emre@outlook.com>
2026-02-08 14:13:34 +0100

    Add optional className prop to HeaderButton

--- frontend/src/components/HeaderButton.tsx
+++ frontend/src/components/HeaderButton.tsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 
-function HeaderButton({ path, text, icon, handleClick }
-  : { path: string; text: string, icon?: string, handleClick: () => void }) {
+interface Props {
+  path: string;
+  text: string;
+  icon?: string;
+  handleClick: () => void;
+  className?: string;
+}
+
+function HeaderButton({ path, text, icon, handleClick, className }: Props) {
 
   return (
-    <li className="nav-item rounded header-button">
+    <li className={`nav-item rounded header-button ${className || ""}`}>
       <Link
         to={path}
         className="nav-link p-3 text-neutral-300 text-decoration-none icon-link"
