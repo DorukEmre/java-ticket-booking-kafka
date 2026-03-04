fb65e02e65057ee2ada16b571674fc20aa50dc8f
Doruk Emre <doruk.emre@outlook.com>
2026-03-04 18:56:15 +0100

    Add context properties

--- backend/gatewayapi/src/test/java/me/doruk/gatewayapi/GatewayapiApplicationTests.java
+++ backend/gatewayapi/src/test/java/me/doruk/gatewayapi/GatewayapiApplicationTests.java
@@ -3,11 +3,16 @@ package me.doruk.gatewayapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "CATALOG_BASE_URL=http://localhost:8081",
+    "CART_BASE_URL=http://localhost:8082",
+    "ORDER_BASE_URL=http://localhost:8083",
+    "app.cors.allowedOrigins=*"
+})
 class GatewayapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
